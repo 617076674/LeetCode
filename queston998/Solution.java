652952832f4cfe7be9dfcf646bb4563e1307b841
qianyihui_i <qianyihui_i@didiglobal.com>
2019-06-12 09:41:33 +0800

    题意不明确

---
+++ queston998/Solution.java
@@ -0,0 +1,29 @@
+package queston998;
+
+/**
+ * @author qianyihui
+ * @date 2019-06-12
+ *
+ * 题意不是非常明确。新插入节点时，始终往右子树中插入。
+ *
+ * 时间复杂度和空间复杂度均是O(h)，其中h为二叉树的高度。
+ *
+ * 执行用时：1ms，击败93.02%。消耗内存：34.3MB，击败93.10%。
+ */
+public class Solution {
+
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        TreeNode treeNode = new TreeNode(val);
+        if (val > root.val) {
+            treeNode.left = root;
+            return treeNode;
+        }
+        if (null == root.right) {
+            root.right = treeNode;
+        } else {
+            root.right = insertIntoMaxTree(root.right, val);
+        }
+        return root;
+    }
+
+}
---
+++ queston998/TreeNode.java
@@ -0,0 +1,10 @@
+package queston998;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
