08498fd9396bcce70fccbaf01d3fa98ae0d3233e
qianyihui_i <qianyihui_i@didiglobal.com>
2019-06-17 21:12:08 +0800

    水题

---
+++ question0412/Solution.java
@@ -0,0 +1,28 @@
+package question0412;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author qianyihui
+ * @date 2019-06-17
+ */
+public class Solution {
+
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+
+}
