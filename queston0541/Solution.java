91218ce90e9295e2d4ad88588f841197ecaae01d
qianyihui_i <qianyihui_i@didiglobal.com>
2019-06-24 09:20:07 +0800

    反转字符串

---
+++ queston0541/Solution.java
@@ -0,0 +1,25 @@
+package queston0541;
+
+/**
+ * @author qianyihui
+ * @date 2019-06-24
+ *
+ * 执行用时：4ms，击败57.28%。消耗内存：41.6MB，击败53.20%。
+ */
+public class Solution {
+    public String reverseStr(String s, int k) {
+        int n = s.length();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < s.length(); i += k) {
+            if (((i / k) & 1) == 1) {
+                //不需要翻转
+                stringBuilder.append(s, i, Math.min(i + k, n));
+            } else {
+                //需要翻转
+                StringBuilder temp = new StringBuilder(s.substring(i, Math.min(i + k, n)));
+                stringBuilder.append(temp.reverse());
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
