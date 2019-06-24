3691e9b1825bdc54879a0e5453eafad4d2536e96
qianyihui_i <qianyihui_i@didiglobal.com>
2019-06-24 09:43:23 +0800

    双指针优化

--- question0011/Solution2.java
+++ question0011/Solution2.java
@@ -1,14 +1,17 @@
-package question011;
+package question0011;
 
-/*
+/**
  * See analysis: https://blog.csdn.net/qq_41231926/article/details/81407474
+ *
+ * 首尾双指针遍历。
+ *
+ * 时间复杂度是O(n)，其中n为height数组的长度。空间复杂度是O(1)。
+ *
+ * 执行用时：6ms，击败78.08%。消耗内存：44.5MB，击败68.02%。
  */
 public class Solution2 {
-	
 	public int maxArea(int[] height) {
-		int n = height.length;
-		int i = 0;
-		int j = n - 1;
+		int n = height.length, i = 0, j = n - 1;
 		int area = (n - 1) * Math.min(height[i], height[j]);
 		while(i < j) {
 			area = Math.max(area, (j - i) * Math.min(height[i], height[j]));
