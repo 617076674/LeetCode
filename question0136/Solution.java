7692d1f1e23b3a9ba19b9fe97edc3701d1f90eb6
qianyihui_i <qianyihui_i@didiglobal.com>
2019-07-05 10:33:22 +0800

    异或位运算

---
+++ question0136/Solution.java
@@ -0,0 +1,18 @@
+package question0136;
+
+/**
+ * 异或位运算。
+ *
+ * 时间复杂度是O(n)，其中n为nums数组的长度。空间复杂度是O(1)。
+ *
+ * 执行用时：1ms，击败99.79%。消耗内存：42.3MB，击败26.52%。
+ */
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+}
--- question0260/Solution.java
+++ question0260/Solution.java
@@ -1,12 +1,19 @@
-package question260;
+package question0260;
 
+/**
+ * 异或位运算。在question0136的基础上做一个区分。
+ *
+ * 时间复杂度是O(n)，其中n为nums数组的长度。空间复杂度是O(1)。
+ *
+ * 执行用时：2ms，击败95.72%。消耗内存：40.5MB，击败37.06%。
+ */
 public class Solution {
     public int[] singleNumber(int[] nums) {
         int num1XORNum2 = 0;
         for (int i = 0; i < nums.length; i++) {
             num1XORNum2 ^= nums[i];
         }
-        num1XORNum2 &= -num1XORNum2;
+        num1XORNum2 &= -num1XORNum2;    //取num1XORNum2最右边的1，其余位均置为0
         int[] result = new int[2];
         result[0] = result[1] = 0;
         for (int i = 0; i < nums.length; i++) {
