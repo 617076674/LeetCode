edd646a50e2ccc3236be47238db8e968587c10f7
qianyihui_i <qianyihui_i@didiglobal.com>
2019-07-05 10:21:41 +0800

    floor和ceil

--- question0074/Solution1.java
+++ question0074/Solution1.java
@@ -1,9 +1,15 @@
-package question074;
+package question0074;
 
-/*
- * See analysis: https://blog.csdn.net/qq_41231926/article/details/82904673
+/**
+ * 先对每行第一列的数据用floor函数进行查找。
+ *
+ * 再对找到的那一行进行二分查找法。
+ *
+ * 时间复杂度是O(log(m) + log(n))，其中m是矩阵的行数，n是矩阵的列数。空间复杂度是O(1)。
+ *
+ * 执行用时：1ms，击败99.30%。消耗内存：38.9MB，击败86.55%。
  */
-public class Solution2 {
+public class Solution1 {
 
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
@@ -16,10 +22,10 @@ public class Solution2 {
         }
         int[] array = new int[m];
         for (int i = 0; i < m; i++) {
-            array[i] = matrix[i][n - 1];
+            array[i] = matrix[i][0];
         }
-        int row = ceil(array, target);
-        if (row == m) {
+        int row = floor(array, target);
+        if (row == -1) {
             return false;
         }
         return binarySearch(matrix[row], target);
@@ -40,22 +46,20 @@ public class Solution2 {
         return false;
     }
 
-    private int ceil(int[] array, int target) {
-        int left = 0;
-        int right = array.length;
+    private int floor(int[] array, int target) {
+        int left = -1, right = array.length - 1;
         while (left < right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left + 1) / 2;
             if (array[mid] < target) {
-                left = mid + 1;
+                left = mid;
             } else {
-                right = mid;
+                right = mid - 1;
             }
         }
-        if (right - 1 >= 0 && array[right - 1] == target) {
-            return right - 1;
-        } else {
-            return right;
+        if (left + 1 < array.length && array[left + 1] == target) {
+            return left + 1;
         }
+        return left;
     }
 
 }
--- question0074/Solution2.java
+++ question0074/Solution2.java
@@ -1,9 +1,15 @@
-package question074;
+package question0074;
 
-/*
- * See analysis: https://blog.csdn.net/qq_41231926/article/details/82904673
+/**
+ * 先对每行最后一列的数据用ceil函数进行查找。
+ *
+ * 再对找到的那一行进行二分查找法。
+ *
+ * 时间复杂度是O(log(m) + log(n))，其中m是矩阵的行数，n是矩阵的列数。空间复杂度是O(1)。
+ *
+ * 执行用时：1ms，击败99.30%。消耗内存：39.8MB，击败76.97%。
  */
-public class Solution1 {
+public class Solution2 {
 
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
@@ -16,10 +22,10 @@ public class Solution1 {
         }
         int[] array = new int[m];
         for (int i = 0; i < m; i++) {
-            array[i] = matrix[i][0];
+            array[i] = matrix[i][n - 1];
         }
-        int row = floor(array, target);
-        if (row == -1) {
+        int row = ceil(array, target);
+        if (row == m) {
             return false;
         }
         return binarySearch(matrix[row], target);
@@ -40,22 +46,20 @@ public class Solution1 {
         return false;
     }
 
-    private int floor(int[] array, int target) {
-        int left = -1;
-        int right = array.length - 1;
+    private int ceil(int[] array, int target) {
+        int left = 0, right = array.length;
         while (left < right) {
-            int mid = left + (right - left + 1) / 2;
+            int mid = left + (right - left) / 2;
             if (array[mid] < target) {
-                left = mid;
+                left = mid + 1;
             } else {
-                right = mid - 1;
+                right = mid;
             }
         }
-        if (left + 1 < array.length && array[left + 1] == target) {
-            return left + 1;
-        } else {
-            return left;
+        if (right - 1 >= 0 && array[right - 1] == target) {
+            return right - 1;
         }
+        return right;
     }
 
 }
