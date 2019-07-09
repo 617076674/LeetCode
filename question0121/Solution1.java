1f8df4d8fc7f674c4eab2a34e180bf7a9460a511
qianyihui_i <qianyihui_i@didiglobal.com>
2019-07-09 18:27:04 +0800

    股票的最大利润

--- question0121/Solution1.java
+++ question0121/Solution1.java
@@ -1,5 +1,12 @@
-package question121;
+package question0121;
 
+/**
+ * 暴力破解法。
+ *
+ * 时间复杂度是O(n ^ 2)，其中n是prices数组的长度。空间复杂度是O(1)。
+ *
+ * 执行用时：804ms，击败5.01%。消耗内存：40MB，击败17.51%。
+ */
 public class Solution1 {
     public int maxProfit(int[] prices) {
         int result = 0;
--- question0121/Solution2.java
+++ question0121/Solution2.java
@@ -1,6 +1,15 @@
-package question121;
+package question0121;
 
-public class Solution3 {
+/**
+ * f(x)：当第x天卖出时能获得的最大利润。
+ *
+ * 只需用一个变量minPrice记录[0, x - 1]天的最小值即可。
+ *
+ * 时间复杂度是O(n)，其中n是prices数组的长度。空间复杂度是O(1)。
+ *
+ * 执行用时：4ms，击败43.49%。消耗内存：38.3MB，击败51.91%。
+ */
+public class Solution2 {
     public int maxProfit(int[] prices) {
         int result = 0;
         if (0 == prices.length) {
