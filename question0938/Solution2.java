be1d5eb21219d2882c0d692bb628ecf15721f59c
qianyihui_i <qianyihui_i@didiglobal.com>
2019-07-11 14:34:04 +0800

    二叉搜索树的范围和-递归

---
+++ question0938/Solution2.java
@@ -0,0 +1,26 @@
+package question0938;
+
+/**
+ * @author qianyihui
+ * @date 2019-07-11
+ *
+ * 递归解决。
+ *
+ * 时间复杂度是O(n)，其中n为树中的节点个数。空间复杂度是O(h)，其中h为树的高度。
+ *
+ * 执行用时：1ms，击败99.70%。消耗内存：54.1MB，击败19.51%。
+ */
+public class Solution2 {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if (null == root) {
+            return 0;
+        }
+        if (root.val > R) {
+            return rangeSumBST(root.left, L, R);
+        }
+        if (root.val < L) {
+            return rangeSumBST(root.right, L, R);
+        }
+        return root.val + rangeSumBST(root.left, L, root.val) + rangeSumBST(root.right, root.val, R);
+    }
+}
