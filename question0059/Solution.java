5816d0e58045ab2e26885430cb3f1a637907887f
qianyihui_i <qianyihui_i@didiglobal.com>
2019-07-12 10:00:27 +0800

    螺旋矩阵II

--- question0059/Solution.java
+++ question0059/Solution.java
@@ -1,17 +1,16 @@
-package question059;
+package question0059;
 
-/*
- * See analysis: https://blog.csdn.net/qq_41231926/article/details/82831145
+/**
+ * 和question0054一样的解法，只不过question0054是取数字，而本题是填数字。
+ *
+ * 时间复杂度是O(n ^ 2)。空间复杂度是O(1)。
+ *
+ * 执行用时：1ms，击败45.11%。消耗内存：35.2MB，击败49.02%。
  */
 public class Solution {
-
     public int[][] generateMatrix(int n) {
         int[][] result = new int[n][n];
-        int num = 1;
-        int left = 0;
-        int right = n - 1;
-        int top = 0;
-        int bottom = n - 1;
+        int num = 1, left = 0, right = n - 1, top = 0, bottom = n - 1;
         while (left <= right && top <= bottom) {
             for (int i = left; i <= right; i++) {
                 result[top][i] = num++;
