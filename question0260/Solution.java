c77f81ebea05a9ce45de72815cc02ee7a5fed9fa
qianyihui_i <qianyihui_i@didiglobal.com>
2019-07-12 10:07:48 +0800

    只出现一次的数III

--- question0260/Solution.java
+++ question0260/Solution.java
@@ -15,7 +15,6 @@ public class Solution {
         }
         num1XORNum2 &= -num1XORNum2;    //取num1XORNum2最右边的1，其余位均置为0
         int[] result = new int[2];
-        result[0] = result[1] = 0;
         for (int i = 0; i < nums.length; i++) {
             if (0 == (nums[i] & num1XORNum2)) {
                 result[0] ^= nums[i];
