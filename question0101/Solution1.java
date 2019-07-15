1fdf10ff7407921d0ddf4e8436ff8db87a91af6d
qianyihui_i <qianyihui_i@didiglobal.com>
2019-07-15 09:15:12 +0800

    对称二叉树-递归/非递归

--- question0101/Solution1.java
+++ question0101/Solution1.java
@@ -1,40 +1,30 @@
 package question0101;
 
 /**
- * 将原树的左子树与翻转后的右子树相比，如果相同，则是对称二叉树。否则，不是对称二叉树。
+ * 递归实现。新建一个函数，判断两个二叉树是否是对称二叉树。
  *
  * 时间复杂度是O(n)，其中n为二叉树中的节点个数。空间复杂度是O(h)，其中h是二叉树的高度。
  *
- * 执行用时：2ms，击败85.03%。消耗内存：36.1MB，击败79.25%。
+ * 执行用时：2ms，击败85.03%。消耗内存：35.3MB，击败81.83%。
  */
 public class Solution1 {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
             return true;
         }
-        return isSameTree(root.left, inverseTree(root.right));
+        return isSymmetric(root.left, root.right);
     }
 
-    private TreeNode inverseTree(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        TreeNode tmp = root.left;
-        root.left = inverseTree(root.right);
-        root.right = inverseTree(tmp);
-        return root;
-    }
-
-    private boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
+    private boolean isSymmetric(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
             return true;
-        } else if (p == null) {
+        } else if (node1 == null) {
             return false;
-        } else if (q == null) {
+        } else if (node2 == null) {
             return false;
-        } else if (p.val != q.val) {
+        } else if (node1.val != node2.val) {
             return false;
         }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return isSymmetric(node1.left, node2.right) && isSymmetric(node1.right, node2.left);
     }
 }
--- question0101/Solution2.java
+++ question0101/Solution2.java
@@ -1,30 +1,39 @@
 package question0101;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- * 递归实现。新建一个函数，判断两个二叉树是否是对称二叉树。
+ * 迭代实现。
  *
  * 时间复杂度是O(n)，其中n为二叉树中的节点个数。空间复杂度是O(h)，其中h是二叉树的高度。
  *
- * 执行用时：2ms，击败85.03%。消耗内存：35.3MB，击败81.83%。
+ * 执行用时：3ms，击败28.99%。消耗内存：35.9MB，击败79.48%。
  */
 public class Solution2 {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
             return true;
         }
-        return isSymmetric(root.left, root.right);
-    }
-
-    private boolean isSymmetric(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) {
-            return true;
-        } else if (node1 == null) {
-            return false;
-        } else if (node2 == null) {
-            return false;
-        } else if (node1.val != node2.val) {
-            return false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll(), node2 = queue.poll();
+            if (node1 == null && node2 == null) {
+                continue;
+            }
+            if (node1 == null || node2 == null) {
+                return false;
+            }
+            if (node1.val != node2.val) {
+                return false;
+            }
+            queue.add(node1.left);
+            queue.add(node2.right);
+            queue.add(node1.right);
+            queue.add(node2.left);
         }
-        return isSymmetric(node1.left, node2.right) && isSymmetric(node1.right, node2.left);
+        return true;
     }
 }
