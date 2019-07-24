1d0acf9b62d0aab987b48b99019782edf4d3a0eb
qianyihui_i <qianyihui_i@didiglobal.com>
2019-07-24 08:29:59 +0800

    神仙看不懂版

--- question0123/Solution5.java
+++ question0123/Solution5.java
@@ -5,7 +5,7 @@ package question0123;
  *
  * 时间复杂度是O(n)，其中n为prices数组的长度。空间复杂度是O(1)。
  *
- * 执行用时：6ms，击败52.56%。消耗内存：38.8MB，击败44.66%
+ * 执行用时：4ms，击败88.52%。消耗内存：39.2MB，击败41.01%
  */
 public class Solution5 {
     public int maxProfit(int[] prices) {
@@ -15,14 +15,10 @@ public class Solution5 {
         }
         int state1 = 0, state2 = -prices[0], state3 = 0, state4 = -prices[0];
         for (int i = 1; i < n; i++) {
-            int newState1 = Math.max(state1, state2 + prices[i]);
-            int newState2 = Math.max(state2, -prices[i]);
-            int newState3 = Math.max(state3, state4 + prices[i]);
-            int newState4 = Math.max(state4, state1 - prices[i]);
-            state1 = newState1;
-            state2 = newState2;
-            state3 = newState3;
-            state4 = newState4;
+            state3 = Math.max(state3, state4 + prices[i]);
+            state4 = Math.max(state4, state1 - prices[i]);
+            state1 = Math.max(state1, state2 + prices[i]);
+            state2 = Math.max(state2, -prices[i]);
         }
         return state3;
     }
