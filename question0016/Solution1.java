907610c08c5891b3af31fbd3dc46ffc0cc5d8ad1
qianyihui_i <qianyihui_i@didiglobal.com>
2019-08-05 09:44:32 +0800

    最接近的三数之和-内层循环用双指针

--- question0016/Solution1.java
+++ question0016/Solution1.java
@@ -1,14 +1,15 @@
-package question016;
+package question0016;
 
-/*
- * See analysis: https://blog.csdn.net/qq_41231926/article/details/82177199
+/**
+ * 暴力破解法。
+ *
+ * 时间复杂度是O(n ^ 3)，其中n为nums数组的长度。空间复杂度是O(1)。
+ *
+ * 执行用时：188ms，击败5.23%。消耗内存：36.9MB，击败83.68%。
  */
 public class Solution1 {
-
     public int threeSumClosest(int[] nums, int target) {
-        int n = nums.length;
-        int diff = Integer.MAX_VALUE;
-        int result = 0;
+        int n = nums.length, diff = Integer.MAX_VALUE, result = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 for (int k = j + 1; k < n; k++) {
--- question0016/Solution2.java
+++ question0016/Solution2.java
@@ -1,20 +1,20 @@
-package question016;
+package question0016;
 
 import java.util.Arrays;
 
-/*
- * See analysis: https://blog.csdn.net/qq_41231926/article/details/82177199
+/**
+ * 和question0015中的Solution3采用同样的思路，内层循环采用双指针遍历的形式。
+ *
+ * 时间复杂度是O(n ^ 2)，其中n为nums数组的长度。空间复杂度是O(1)。
+ *
+ * 执行用时：17ms，击败48.29%。消耗内存：36.5MB，击败84.07%。
  */
 public class Solution2 {
-
     public int threeSumClosest(int[] nums, int target) {
-        int n = nums.length;
-        int diff = Integer.MAX_VALUE;
-        int result = 0;
+        int n = nums.length, diff = Integer.MAX_VALUE, result = 0;
         Arrays.sort(nums);
         for (int i = 0; i < n - 2; i++) {
-            int left = i + 1;
-            int right = n - 1;
+            int left = i + 1, right = n - 1;
             while (left < right) {
                 if (nums[i] + nums[left] + nums[right] < target) {
                     if (Math.abs(nums[i] + nums[left] + nums[right] - target) < diff) {
