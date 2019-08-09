c6ea31bf59b167379eeafb5113bad4dfc150a868
qianyihui_i <qianyihui_i@didiglobal.com>
2019-08-09 14:47:47 +0800

    二分查找-神奇的二分查找法模板

--- question0704/Solution.java
+++ question0704/Solution.java
@@ -12,13 +12,11 @@ public class Solution {
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left < right) {
-            int mid = left + ((right - left) >> 1);
-            if (target == nums[mid]) {
-                return mid;
-            } else if (target < nums[mid]) {
+            int mid = (left + right + 1) >>> 1;
+            if (target < nums[mid]) {
                 right = mid - 1;
             } else {
-                left = mid + 1;
+                left = mid;
             }
         }
         if (nums[left] == target) {
