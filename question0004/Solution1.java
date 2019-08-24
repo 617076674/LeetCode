7de293335e03f3b325ca03afae6df4703687a756
钱一辉 <617076674@qq.com>
2019-08-24 09:14:50 +0800

    寻找两个有序数组的中位数-二分查找法

--- question0004/Solution1.java
+++ question0004/Solution1.java
@@ -1,35 +1,38 @@
 package question0004;
 
 /**
- * 找出前len个数。
+ * 寻找第k个数和第k - 1个数
  *
- * 时间复杂度和空间复杂度均是O(n1 + n2)，其中n1是nums1数组的长度，n2是nums2数组的长度。
+ * 时间复杂度是O(n1 + n2)，其中n1是nums1数组的长度，n2是nums2数组的长度。空间复杂度是O(1)。
  *
  * 执行用时：12ms，击败86.12%。消耗内存：52.8MB，击败69.61%。
  */
 public class Solution1 {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
-        int len = ((n1 + n2) >> 1) + 1;
-        int[] array = new int[len];
+        int n1 = nums1.length, n2 = nums2.length, n = n1 + n2;
+        //如果(n1 + n2)是一个偶数，比如n1 = 2，n2 = 2，那么中位数是索引为1和2的两个数的平均
+        //如果(n1 + n2)是一个奇数，比如n1 = 1，n2 = 2，那么中位数是索引为1的数
+        //无论是奇数还是偶数，我们寻找索引为(((n1 + n2) >> 1) + 1)的那个数，同时记录其前一个数，当(n1 + n2)是偶数时需要用到
         int i = 0, j = 0, index = 0;
-        while (index < array.length) {
+        Integer pre = null, cur = null;
+        while (index < ((n >> 1) + 1)) {
+            pre = cur;
             if (i < n1 && j < n2) {
                 if (nums1[i] < nums2[j]) {
-                    array[index] = nums1[i++];
+                    cur = nums1[i++];
                 } else {
-                    array[index] = nums2[j++];
+                    cur = nums2[j++];
                 }
             } else if (i >= n1 && j < n2) {
-                array[index] = nums2[j++];
+                cur = nums2[j++];
             } else if (i < n1) {
-                array[index] = nums1[i++];
+                cur = nums1[i++];
             }
             index++;
         }
-        if (((n1 + n2) & 1) == 0) {
-            return 0.5 * (array[len - 1] + array[len - 2]);
+        if ((n & 1) == 0) {
+            return 0.5 * (cur + pre);
         }
-        return array[len - 1];
+        return cur;
     }
 }
