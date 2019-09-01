6c0eca1be7391b4033a444e8e51c300010685c87
钱一辉 <617076674@qq.com>
2019-09-01 08:40:47 +0800

    整数转罗马数字

--- question0012/Solution.java
+++ question0012/Solution.java
@@ -1,20 +1,23 @@
 package question0012;
 
-/*
- * See analysis: https://blog.csdn.net/qq_41231926/article/details/82012326
+/**
+ * 将特殊的数字与罗马字符一一对应起来。
+ *
+ * 时间复杂度是O(num)。空间复杂度是O(1)。
+ *
+ * 执行用时：11ms，击败81.11%。消耗内存：38.8MB，击败95.56%。
  */
-public class Solution2 {
-
+public class Solution {
     public String intToRoman(int num) {
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] strings = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < values.length; i++) {
             while (num >= values[i]) {
-                stringBuilder.append(strings[i]);
+                sb.append(strings[i]);
                 num -= values[i];
             }
         }
-        return stringBuilder.toString();
+        return sb.toString();
     }
 }
