bd4a93e44997f29574a3e3e29cb57706e303e9d6
钱一辉 <617076674@qq.com>
2019-09-02 09:40:45 +0800

    最长回文子序列-动态规划

---
+++ question0516/Solution.java
@@ -0,0 +1,37 @@
+package question0516;
+
+/**
+ * 动态规划。
+ *
+ * 状态定义：
+ * dp[i][j]:字符串s中[i, j]范围内的子串中的最长回文序列长度。
+ *
+ * 状态转移：
+ * 当i == j时，dp[i][j] = 1。
+ * 当i < j时，如果s.charAt(i) == s.charAt(j)，那么dp[i][j] = dp[i + 1][j - 1] + 2。
+ *           如果s.charAt(i) != s.charAt(j)，那么dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])。
+ *
+ * 时间复杂度和空间复杂度均是O(n ^ 2)，其中n为字符串s的长度。
+ *
+ * 执行用时：61ms，击败28.97%。消耗内存：54.3MB，击败6.00%。
+ */
+public class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int gap = -1; gap >= 1 - n; gap--) {
+            for (int i = 0; i < n + gap; i++) {
+                int j = i - gap;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
