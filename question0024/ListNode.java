d5482c1be25a7b84ee5b4330fd8bebff4cb5d612
钱一辉 <617076674@qq.com>
2019-09-13 16:22:48 +0800

    两两交换链表中的节点-三指针、递归

--- question0024/ListNode.java
+++ question0024/ListNode.java
@@ -1,9 +1,9 @@
-package question024;
+package question0024;
 
 public class ListNode {
-
     public int val;
-    public ListNode next = null;
+
+    public ListNode next;
 
     public ListNode(int x) {
         val = x;
--- question0024/Solution1.java
+++ question0024/Solution1.java
@@ -1,19 +1,22 @@
-package question024;
+package question0024;
 
-/*
- * See analysis: https://blog.csdn.net/qq_41231926/article/details/82377317
+/**
+ * 三指针。
+ *
+ * 时间复杂度是O(n)，其中n为链表的长度。空间复杂度是O(1)。
+ *
+ * 执行用时：0ms，击败100.00%。消耗内存：35.1MB，击败63.60%。
  */
-public class Solution {
-
+public class Solution1 {
     public ListNode swapPairs(ListNode head) {
         ListNode dummyHead = new ListNode(-1);
         dummyHead.next = head;
         ListNode cur1 = dummyHead;
+        //如果链表中只有0个或者1个节点，无需进行任何交换操作
         if (cur1.next == null || cur1.next.next == null) {
             return dummyHead.next;
         }
-        ListNode cur2 = cur1.next;
-        ListNode cur3 = cur2.next;
+        ListNode cur2 = cur1.next, cur3 = cur2.next;
         while (true) {
             cur2.next = cur3.next;
             cur1.next = cur3;
