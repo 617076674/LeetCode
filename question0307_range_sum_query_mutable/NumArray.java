f5463f3d999586a7afed8f41c4d684ea4fc7a36c
钱一辉 <617076674@qq.com>
2019-09-25 22:16:07 +0800

    区域和检索-数组可修改

--- question0307_range_sum_query_mutable/NumArray.java
+++ question0307_range_sum_query_mutable/NumArray.java
@@ -1,16 +1,24 @@
-package question307;
+package question0307_range_sum_query_mutable;
 
+/**
+ * 线段树。
+ *
+ * sumRange()的时间复杂度是O(logn)，其中n为nums数组中的元素个数。
+ * update()的时间复杂度也是O(logn)。
+ *
+ * 执行用时：92ms，击败97.18%。消耗内存：46.1MB，击败99.18%。
+ */
 public class NumArray {
     private class SegmentTree {
-        private Integer[] tree;
-        private Integer[] data;
+        private int[] tree;
+        private int[] data;
 
-        public SegmentTree(Integer[] arr) {
-            data = new Integer[arr.length];
+        public SegmentTree(int[] arr) {
+            data = new int[arr.length];
             for (int i = 0; i < arr.length; i++) {
                 data[i] = arr[i];
             }
-            tree = new Integer[4 * arr.length];
+            tree = new int[4 * arr.length];
             buildSegmentTree(0, 0, data.length - 1);
         }
 
@@ -22,74 +30,49 @@ public class NumArray {
                 tree[treeIndex] = data[left];
                 return;
             }
-            int leftTreeIndex = leftChild(treeIndex);
-            int rightTreeIndex = rightChild(treeIndex);
-            int mid = left + (right - left) / 2;            //为了防止数据越界！！！
+            int leftTreeIndex = 2 * treeIndex + 1, rightTreeIndex = 2 * treeIndex + 2, mid = left + ((right - left) >> 1);            //为了防止数据越界！！！
             buildSegmentTree(leftTreeIndex, left, mid);
             buildSegmentTree(rightTreeIndex, mid + 1, right);
             tree[treeIndex] = tree[leftTreeIndex] + tree[rightTreeIndex];
         }
 
         /*
-         * 返回满二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
-         */
-        private int leftChild(int index) {
-            return 2 * index + 1;
-        }
-
-        /*
-         * 返回满二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
-         */
-        private int rightChild(int index) {
-            return 2 * index + 2;
-        }
-
-        /*
-         * 在线段树中所涉及的操作不需要通过一个节点去寻找它的父亲节点，因此不需要parent()这个函数
-         */
-        /*
          * 返回区间[queryL, queryR]的值
          */
-        public Integer query(int queryL, int queryR) {
+        public int query(int queryL, int queryR) {
             return query(0, 0, data.length - 1, queryL, queryR);
         }
 
         /*
          * 在以treeID为根的线段树中[left...right]的范围里，搜索区间[queryL...queryR]的值
          */
-        private Integer query(int treeIndex, int left, int right, int queryL, int queryR) {
+        private int query(int treeIndex, int left, int right, int queryL, int queryR) {
             if (left == queryL && right == queryR) {
                 return tree[treeIndex];
             }
-            int mid = left + (right - left) / 2;
-            int leftTreeIndex = leftChild(treeIndex);
-            int rightTreeIndex = rightChild(treeIndex);
+            int mid = left + ((right - left) >> 1), leftTreeIndex = 2 * treeIndex + 1, rightTreeIndex = 2 * treeIndex + 2;
             if (queryL >= mid + 1) {
                 return query(rightTreeIndex, mid + 1, right, queryL, queryR);
-            } else if (queryR <= mid) {
-                return query(leftTreeIndex, left, mid, queryL, queryR);
-            } else {
-                Integer leftResult = query(leftTreeIndex, left, mid, queryL, mid);
-                Integer rightResult = query(rightTreeIndex, mid + 1, right, mid + 1, queryR);
-                return leftResult + rightResult;
             }
+            if (queryR <= mid) {
+                return query(leftTreeIndex, left, mid, queryL, queryR);
+            }
+            return query(leftTreeIndex, left, mid, queryL, mid) + query(rightTreeIndex, mid + 1, right, mid + 1, queryR);
         }
 
         //将index位置的值，更新为e
-        public void update(int i, Integer val) {
+        public void update(int i, int val) {
             data[i] = val;
             update(0, 0, data.length - 1, i, val);
         }
 
         //在以treeIndex为根的线段树中更新index的值为e
-        private void update(int treeIndex, int left, int right, int index, Integer e) {
+        private void update(int treeIndex, int left, int right, int index, int e) {
             if (left == right) {
                 tree[treeIndex] = e;
                 return;
             }
-            int mid = (right - left) / 2 + left;
-            int leftTreeIndex = leftChild(treeIndex);
-            int rightTreeIndex = rightChild(treeIndex);
+            int mid = left + ((right - left) >> 1), leftTreeIndex = 2 * treeIndex + 1, rightTreeIndex = 2 * treeIndex + 2;
             if (index >= mid + 1) {
                 update(rightTreeIndex, mid + 1, right, index, e);
             } else {
@@ -102,9 +85,10 @@ public class NumArray {
     private SegmentTree segmentTree;
 
     public NumArray(int[] nums) {
-        if (nums.length > 0) {
-            Integer[] data = new Integer[nums.length];    //int[]是不能自动包装转换为Integer[]的！！！
-            for (int i = 0; i < nums.length; i++) {
+        int n = nums.length;
+        if (n > 0) {
+            int[] data = new int[n];    //int[]是不能自动包装转换为Integer[]的！！！
+            for (int i = 0; i < n; i++) {
                 data[i] = nums[i];
             }
             segmentTree = new SegmentTree(data);
